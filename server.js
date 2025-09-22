36684a59f193a5a92ddd5aa09667f189196c8090
galaxy76709 <kayky.gabriel@sga.pucminas.br>
2025-09-22 16:03:22 -0300

    Changes in server.js

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require ('express')
+const express = require('express');
 const cors = require('cors');
-const path = require ('path')
-const app = express ()
+const path = require('path');
+const app = express();
 
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/app', express.static (path.join (__dirname, '/public')))
+// --- Rotas ---
 
-let port = process.env.PORT || 3000
-app.listen (port)
+app.use('/app', express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => 
+{res.send('<h1>API está no ar!</h1><p>Bem-vindo ao meu projeto.</p>');
+});
+
+
+// --- Inicialização do Servidor ---
+// Documentação: Esta parte inicia o servidor e o faz "ouvir" por pedidos na porta definida.
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  // Uma mensagem no terminal para sabermos que o servidor iniciou com sucesso.
+  console.log(`Servidor a correr na porta ${port}`);
+});
